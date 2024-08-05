fb7fbaa35dccf6e9274124a6385e2a23d703c263
🦅KoNekoD <moskva111@yahoo.com>
2024-08-05 19:04:43 +0300

    fix: values

--- cmd/KSortMap/main.go
+++ cmd/KSortMap/main.go
@@ -3,7 +3,7 @@ package KSortMap
 import "sort"
 
 // Sort - sorts a map by its keys
-func Sort[K ~int | ~int64 | ~string, V comparable](input map[K]V) map[K]V {
+func Sort[K ~int | ~int64 | ~string, V any](input map[K]V) map[K]V {
 	keys := make([]K, 0, len(input))
 	for k := range input {
 		keys = append(keys, k)
